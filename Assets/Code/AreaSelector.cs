4ff2901768eefa49dc98469c187e609ce1ebd1b2
Marian Kleineberg <mail@marian42.de>
2018-10-25 23:27:23 +0200

    Add area selector
    Former-commit-id: dbf87654e3f786ecc0270800f2bd4562ae7442ce

---
+++ Assets/Code/AreaSelector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AreaSelector : MonoBehaviour {
+	public MapGenerator MapGenerator;
+
+	public Vector3i StartPosition {
+		get {
+			var start = new Vector3i((this.transform.position) / MapGenerator.BlockSize);
+			if (start.Y > this.MapGenerator.HeightLimit) {
+				start.Y = this.MapGenerator.HeightLimit;
+			}
+			if (start.Y < -this.MapGenerator.HeightLimit) {
+				start.Y = -this.MapGenerator.HeightLimit;
+			}
+			return start;
+		}
+	}
+
+	public Vector3i Size {
+		get {
+			var start = this.StartPosition;
+			var size = new Vector3i(this.transform.localScale / MapGenerator.BlockSize);
+			if (size.Y + start.Y > this.MapGenerator.HeightLimit) {
+				size.Y = System.Math.Max(0, this.MapGenerator.HeightLimit - start.Y);
+			}
+			return size;
+		}
+	}
+
+//#if UNITY_EDITOR
+	[DrawGizmo(GizmoType.InSelectionHierarchy | GizmoType.NotInSelectionHierarchy)]
+	static void DrawGizmoForMyScript(AreaSelector areaSelector, GizmoType gizmoType) {
+		var size = areaSelector.Size.ToVector3() * MapGenerator.BlockSize;
+		var start = areaSelector.StartPosition.ToVector3() * MapGenerator.BlockSize - new Vector3(1f, 0, 1f) * MapGenerator.BlockSize * 0.5f;
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(areaSelector.MapGenerator.transform.position + start + size * 0.5f, size);
+	}
+//#endif
+}
---
+++ Assets/Editor/AreaSelectorEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AreaSelector))]
+public class AreaSelectorEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		AreaSelector selector = (AreaSelector)target;
+		if (GUILayout.Button("Generate")) {
+			selector.MapGenerator.Collapse(selector.StartPosition, selector.Size);
+		}
+	}
+}
