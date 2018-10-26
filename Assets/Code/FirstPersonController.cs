d805f125dc34d8e3e53c8cf99dc9044117e5eb0c
Marian Kleineberg <mail@marian42.de>
2018-10-26 23:39:22 +0200

    Tweak first FirstPersonController

--- Assets/Code/FirstPersonController.cs
+++ Assets/Code/FirstPersonController.cs
@@ -27,7 +27,8 @@ public class FirstPersonController : MonoBehaviour {
 	}
 	
 	void Update () {
-		this.characterController.Move(this.transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * this.MovementSpeed + this.transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * this.MovementSpeed);
+		float runMultiplier = Input.GetKey(KeyCode.LeftShift) ? 2f : 1f;
+		this.characterController.Move(this.transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * this.MovementSpeed * runMultiplier + this.transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * this.MovementSpeed * runMultiplier);
 		this.transform.rotation = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * Time.deltaTime * this.LookSensitivity, Vector3.up) * this.transform.rotation;
 		this.cameraTilt = Mathf.Clamp(this.cameraTilt - Input.GetAxis("Mouse Y") * this.LookSensitivity * Time.deltaTime, -90f, 90f);
 		this.cameraTransform.localRotation = Quaternion.AngleAxis(this.cameraTilt, Vector3.right);
@@ -40,7 +41,7 @@ public class FirstPersonController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			this.jump();
 		}
-		if (Input.GetKey(KeyCode.LeftShift)) {
+		if (Input.GetKey(KeyCode.LeftControl)) {
 			this.verticalSpeed = 2f;
 		}
 		this.characterController.Move(Vector3.up * Time.deltaTime * this.verticalSpeed);
@@ -48,7 +49,7 @@ public class FirstPersonController : MonoBehaviour {
 
 	private bool onGround() {
 		var ray = new Ray(this.transform.position, Vector3.down);
-		return Physics.Raycast(ray, this.characterController.height / 2 + 0.1f);
+		return Physics.SphereCast(ray, this.characterController.radius, this.characterController.height / 2 + 0.1f);
 	}
 
 	private void jump() {
