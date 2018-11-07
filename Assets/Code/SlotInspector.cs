7ace1541125524318cb2838e04de06d9c1766b41
Marian Kleineberg <mail@marian42.de>
2018-11-08 00:58:40 +0100

    Add SlotInspector

---
+++ Assets/Code/SlotInspector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SlotInspector : MonoBehaviour {
+
+	public MapGenerator MapGenerator;
+
+	public Vector3i GetPosition() {
+		return new Vector3i(this.transform.position / MapGenerator.BlockSize + new Vector3(-1, 0, 1) * 0.5f);
+	}
+
+#if UNITY_EDITOR
+	[DrawGizmo(GizmoType.Selected)]
+	static void DrawGizmoForMyScript(SlotInspector target, GizmoType gizmoType) {
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireCube(target.MapGenerator.GetWorldspacePosition(target.GetPosition()), Vector3.one * MapGenerator.BlockSize);
+	}
+#endif
+}
---
+++ Assets/Editor/SlotInspectorEditor.cs
@@ -0,0 +1,77 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(SlotInspector))]
+public class SlotInspectorEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		SlotInspector slotInspector = (SlotInspector)target;
+		var mapGenerator = slotInspector.MapGenerator;
+
+		if (mapGenerator == null || Application.isPlaying) {
+			return;
+		}
+
+		if (!mapGenerator.Initialized) {
+			if (GUILayout.Button("Initialize Map")) {
+				mapGenerator.Initialize();
+			}
+			return;
+		}
+
+		var position = slotInspector.GetPosition();
+		GUILayout.Label("Position: " + position);
+
+		var slot = slotInspector.MapGenerator.GetSlot(position, false);
+
+		if (slot == null) {
+			if (GUILayout.Button("CreateSlot")) {
+				mapGenerator.GetSlot(position);
+			}
+			return;
+		}
+
+		if (slot.Collapsed) {			
+			GUILayout.Label("Collapsed: " + slot.Module);
+			return;
+		}
+
+		GUILayout.Label("Possible modules: " + slot.Modules.Count() + " / " + mapGenerator.Modules.Count());
+
+		if (GUILayout.Button("Collapse Random")) {
+			slot.CollapseRandom();
+			mapGenerator.BuildAllSlots();
+		}
+
+		var prototypes = new Dictionary<ModulePrototype, List<Module>>();
+		
+		foreach (var module in slot.Modules.ToArray()) {
+			var proto = module.Prototype;
+			if (!prototypes.ContainsKey(proto)) {
+				prototypes[proto] = new List<Module>();
+			}
+			prototypes[proto].Add(module);
+		}
+
+		foreach (var proto in prototypes.Keys) {
+			var list = prototypes[proto];
+			
+			GUILayout.BeginHorizontal();
+
+			EditorGUILayout.PrefixLabel(proto.gameObject.name);
+			foreach (var module in list) {
+				if (GUILayout.Button("R" + module.Rotation)) {
+					slot.Collapse(module);
+					mapGenerator.BuildAllSlots();
+				}
+			}
+
+			GUILayout.EndHorizontal();
+		}		
+	}
+}
