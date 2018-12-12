424dba63d905f2f645c33840e0ce73becd5f5e08
Marian Kleineberg <mail@marian42.de>
2018-12-12 20:21:52 +0100

    Tweak SlotInspector GUI

--- Assets/Editor/SlotInspectorEditor.cs
+++ Assets/Editor/SlotInspectorEditor.cs
@@ -7,35 +7,7 @@ using System.Collections.Generic;
 [CustomEditor(typeof(SlotInspector))]
 public class SlotInspectorEditor : Editor {
 
-	public override void OnInspectorGUI() {
-		DrawDefaultInspector();
-
-		SlotInspector slotInspector = (SlotInspector)target;
-		var mapBehaviour = slotInspector.MapBehaviour;
-		var map = mapBehaviour.Map;
-		if (mapBehaviour == null) {
-			return;
-		}
-
-		var position = slotInspector.GetPosition();
-		GUILayout.Label("Position: " + position);
-
-		if (!mapBehaviour.Initialized) {
-			if (GUILayout.Button("Initialize Map")) {
-				mapBehaviour.Initialize();
-			}
-			return;
-		}
-
-		var slot = map.GetSlot(position, false);
-
-		if (slot == null) {
-			if (GUILayout.Button("CreateSlot")) {
-				map.GetSlot(position);
-			}
-			return;
-		}
-
+	private void showEditor(Slot slot, MapBehaviour mapBehaviour) {
 		if (slot.Collapsed) {
 			GUILayout.Label("Collapsed: " + slot.Module);
 			GUILayout.Space(20f);
@@ -53,7 +25,7 @@ public class SlotInspectorEditor : Editor {
 		}
 
 		var prototypes = new Dictionary<ModulePrototype, List<Module>>();
-		
+
 		foreach (var module in slot.Modules.ToArray()) {
 			var proto = module.Prototype;
 			if (!prototypes.ContainsKey(proto)) {
@@ -64,7 +36,7 @@ public class SlotInspectorEditor : Editor {
 
 		foreach (var proto in prototypes.Keys) {
 			var list = prototypes[proto];
-			
+
 			GUILayout.BeginHorizontal();
 
 			EditorGUILayout.PrefixLabel(proto.gameObject.name + " (" + (100f * list.Sum(module => module.Prototype.Probability) / slot.Modules.Sum(module => module.Prototype.Probability)).ToString("0.0") + "%)");
@@ -76,6 +48,45 @@ public class SlotInspectorEditor : Editor {
 			}
 
 			GUILayout.EndHorizontal();
-		}		
+		}
+	}
+
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		SlotInspector slotInspector = (SlotInspector)target;
+		var mapBehaviour = slotInspector.MapBehaviour;
+		var map = mapBehaviour.Map;
+		if (mapBehaviour == null) {
+			return;
+		}
+
+		var position = slotInspector.GetPosition();
+		GUILayout.Label("Position: " + position);
+
+		if (!mapBehaviour.Initialized) {
+			if (GUILayout.Button("Initialize Map")) {
+				mapBehaviour.Initialize();
+				mapBehaviour.Map.GetSlot(position);
+			}
+			return;
+		}
+
+		if (GUILayout.Button("Reset Map")) {
+			mapBehaviour.Clear();
+			mapBehaviour.Initialize();
+			mapBehaviour.Map.GetSlot(position);
+		}
+		GUILayout.Space(10f);
+
+		var slot = map.GetSlot(position, false);
+
+		if (slot == null) {
+			if (GUILayout.Button("CreateSlot")) {
+				map.GetSlot(position);
+			}
+		} else {
+			this.showEditor(slot, mapBehaviour);
+		}
 	}
 }
