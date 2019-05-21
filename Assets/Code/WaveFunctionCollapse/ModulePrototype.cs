aced2cf4338f41772adb237488b3c4e0280b8859
Marian Kleineberg <mail@marian42.de>
2019-05-21 20:26:30 +0200

    Show possible neighbors when editing module prototypes

--- Assets/Code/WaveFunctionCollapse/ModulePrototype.cs
+++ Assets/Code/WaveFunctionCollapse/ModulePrototype.cs
@@ -82,22 +82,56 @@ public class ModulePrototype : MonoBehaviour {
 		}
 	}
 
-	public Mesh GetMesh() {
+	public Mesh GetMesh(bool createEmptyFallbackMesh = true) {
 		var meshFilter = this.GetComponent<MeshFilter>();
 		if (meshFilter != null && meshFilter.sharedMesh != null) {
 			return meshFilter.sharedMesh;
 		}
-		var mesh = new Mesh();
-		return mesh;
+		if (createEmptyFallbackMesh) {
+			var mesh = new Mesh();
+			return mesh;
+		}
+		return null;
 	}
-
-	private static GUIStyle style;
-
+	
 	
 #if UNITY_EDITOR
+	private static ModulePrototypeEditorData editorData;
+	private static GUIStyle style;
+
 	[DrawGizmo(GizmoType.InSelectionHierarchy | GizmoType.NotInSelectionHierarchy)]
 	static void DrawGizmo(ModulePrototype modulePrototype, GizmoType gizmoType) {
-		Vector3 position = modulePrototype.transform.position;
+		var transform = modulePrototype.transform;
+		Vector3 position = transform.position;
+		var rotation = transform.rotation;
+
+		if (ModulePrototype.editorData == null || ModulePrototype.editorData.ModulePrototype != modulePrototype) {
+			ModulePrototype.editorData = new ModulePrototypeEditorData(modulePrototype);
+		}
+
+		Gizmos.color = new Color(1f, 1f, 1f, 0.3f);
+		if ((gizmoType & GizmoType.Selected) != 0) {
+			for (int i = 0; i < 6; i++) {
+				var hint = ModulePrototype.editorData.GetConnectorHint(i);
+				if (hint.Mesh != null) {
+					Gizmos.DrawMesh(hint.Mesh,
+						position + rotation * Orientations.Direction[i].ToVector3() * 2f,
+						rotation * Quaternion.Euler(Vector3.up * 90f * hint.Rotation));
+				}
+			}
+		}
+		for (int i = 0; i < 6; i++) {	
+			if (modulePrototype.Faces[i].Walkable) {
+				Gizmos.color = Color.red;
+				Gizmos.DrawLine(position + Vector3.down * 0.1f, position + rotation * Orientations.Rotations[i] * Vector3.forward + Vector3.down * 0.1f);
+			}
+			if (modulePrototype.Faces[i].IsOcclusionPortal) {
+				Gizmos.color = Color.blue;
+
+				var dir = rotation * Orientations.Rotations[i] * Vector3.forward;
+				Gizmos.DrawWireCube(position + dir, (Vector3.one - new Vector3(Mathf.Abs(dir.x), Mathf.Abs(dir.y), Mathf.Abs(dir.z))) * 2f);
+			}			
+		}
 
 		if (ModulePrototype.style == null) {
 			ModulePrototype.style = new GUIStyle();
@@ -107,20 +141,7 @@ public class ModulePrototype : MonoBehaviour {
 		ModulePrototype.style.normal.textColor = Color.black;
 		for (int i = 0; i < 6; i++) {
 			var face = modulePrototype.Faces[i];
-			Handles.Label(position + modulePrototype.transform.rotation * Orientations.Rotations[i] * Vector3.forward * InfiniteMap.BLOCK_SIZE / 2f, face.ToString(), ModulePrototype.style);
-		}
-
-		for (int i = 0; i < 6; i++) {
-			if (modulePrototype.Faces[i].Walkable) {
-				Gizmos.color = Color.red;
-				Gizmos.DrawLine(modulePrototype.transform.position + Vector3.down * 0.1f, modulePrototype.transform.position + modulePrototype.transform.rotation * Orientations.Rotations[i] * Vector3.forward + Vector3.down * 0.1f);
-			}
-			if (modulePrototype.Faces[i].IsOcclusionPortal) {
-				Gizmos.color = Color.blue;
-
-				var dir = modulePrototype.transform.rotation * Orientations.Rotations[i] * Vector3.forward;
-				Gizmos.DrawWireCube(modulePrototype.transform.position + dir, (Vector3.one - new Vector3(Mathf.Abs(dir.x), Mathf.Abs(dir.y), Mathf.Abs(dir.z))) * 2f);
-			}
+			Handles.Label(position + rotation * Orientations.Rotations[i] * Vector3.forward * InfiniteMap.BLOCK_SIZE / 2f, face.ToString(), ModulePrototype.style);
 		}
 	}
 #endif
---
+++ Assets/Code/WaveFunctionCollapse/ModulePrototypeEditorData.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ModulePrototypeEditorData {
+	public readonly ModulePrototype ModulePrototype;
+
+	private readonly ModulePrototype[] prototypes;
+
+	private readonly Dictionary<ModulePrototype, Mesh> meshes;
+
+	public struct ConnectorHint {
+		public readonly Mesh Mesh;
+		public readonly int Rotation;
+
+		public ConnectorHint(int rotation, Mesh mesh) {
+			this.Rotation = rotation;
+			this.Mesh = mesh;
+		}
+	}
+
+	public ModulePrototypeEditorData(ModulePrototype modulePrototype) {
+		this.ModulePrototype = modulePrototype;
+		this.prototypes = modulePrototype.transform.parent.GetComponentsInChildren<ModulePrototype>();
+		this.meshes = new Dictionary<ModulePrototype, Mesh>();
+	}
+
+	private Mesh getMesh(ModulePrototype modulePrototype) {
+		if (this.meshes.ContainsKey(modulePrototype)) {
+			return this.meshes[modulePrototype];
+		}
+		var mesh = modulePrototype.GetMesh(false);
+		this.meshes[modulePrototype] = mesh;
+		return mesh;
+	}
+
+	public ConnectorHint GetConnectorHint(int direction) {
+		var face = this.ModulePrototype.Faces[direction];
+		if (face is ModulePrototype.HorizontalFaceDetails) {
+			var horizontalFace = face as ModulePrototype.HorizontalFaceDetails;
+			
+			foreach (var prototype in this.prototypes) {
+				if (prototype == this.ModulePrototype || face.ExcludedNeighbours.Contains(prototype)) {
+					continue;
+				}
+				for (int rotation = 0; rotation < 4; rotation++) {
+					var otherFace = prototype.Faces[Orientations.Rotate(direction, rotation + 2)] as ModulePrototype.HorizontalFaceDetails;
+					if (otherFace.ExcludedNeighbours.Contains(this.ModulePrototype)) {
+						continue;
+					}
+					if (otherFace.Connector == face.Connector && ((horizontalFace.Symmetric && otherFace.Symmetric) || otherFace.Flipped != horizontalFace.Flipped)) {
+						return new ConnectorHint(rotation, this.getMesh(prototype));
+					}
+				}
+			}
+		}
+
+		if (face is ModulePrototype.VerticalFaceDetails) {
+			var verticalFace = face as ModulePrototype.VerticalFaceDetails;
+
+			foreach (var prototype in this.prototypes) {
+				if (prototype == this.ModulePrototype || face.ExcludedNeighbours.Contains(prototype)) {
+					continue;
+				}
+				var otherFace = prototype.Faces[(direction + 3) % 6] as ModulePrototype.VerticalFaceDetails;
+				if (otherFace.ExcludedNeighbours.Contains(this.ModulePrototype) || otherFace.Connector != face.Connector) {
+					continue;
+				}
+
+				return new ConnectorHint(verticalFace.Rotation - otherFace.Rotation, this.getMesh(prototype));
+			}
+		}
+
+		return new ConnectorHint();
+	}
+}
