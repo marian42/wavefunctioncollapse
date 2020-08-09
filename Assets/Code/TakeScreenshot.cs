14f9f79839ffd6b5edd3dc956f1a7e84d962abe0
Marian Kleineberg <mail@marian42.de>
2020-08-09 13:19:48 +0200

    Add screenshot script

---
+++ Assets/Code/TakeScreenshot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class TakeScreenshot : MonoBehaviour {
+    private Camera screenshotCamera;
+
+    public KeyCode Key;
+
+    public int Counter;
+
+    public string Filename;
+    
+    public bool CaptureOnClick = false;
+
+    void Update() {
+        if (Input.GetKeyDown(this.Key)) {
+            this.Capture();
+        }
+
+        if (this.CaptureOnClick && Input.GetMouseButtonDown(0)) {
+            this.Capture();
+        }
+    }
+
+    private string getFilename() {
+        return this.Filename.Replace("{0}", this.Counter.ToString("D4"));
+    }
+
+    public void Capture() {
+        if (this.screenshotCamera == null) {
+            this.screenshotCamera = this.GetComponent<Camera>();
+        }
+
+        while (System.IO.File.Exists(this.getFilename())) {
+            this.Counter++;
+        }
+
+        ScreenCapture.CaptureScreenshot(this.getFilename());
+        Debug.Log(this.getFilename());
+        this.Counter++;
+    }
+}
